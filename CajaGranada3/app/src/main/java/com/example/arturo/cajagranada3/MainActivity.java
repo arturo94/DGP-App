682a02d3a37e3fd8f6267af39eb62acf39d176b0
DESKTOP-R96S2DN\Brb-Portable <brbcode@gmail.com>
2017-12-21 16:22:55 +0100

    Reader and Portait

--- CajaGranada3/app/src/main/java/com/example/arturo/cajagranada3/MainActivity.java
+++ CajaGranada3/app/src/main/java/com/example/arturo/cajagranada3/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.arturo.cajagranada3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         scanner = (Button) findViewById(R.id.buttonMain);
 
